6623026cb2d8afb1f45e703385a77fd9153039b1
katyusya <katusha.fedyakena@mail.ru>
2023-10-19 18:17:47 +0300

    Добавление Parameter и Description

--- src/components/UI/DescriptionCurrentDate/DescriptionCurrentDate.tsx
+++ src/components/UI/DescriptionCurrentDate/DescriptionCurrentDate.tsx
@@ -1,21 +1,69 @@
 import Parametr from '../Parameter/Parameter';
 import style from './DescriptionCurrentDate.module.scss';
 
+import parameter1 from '@assets/images/Parameter/parameter1.svg';
+import parameter2 from '@assets/images/Parameter/parameter2.svg';
+import parameter3 from '@assets/images/Parameter/parameter3.svg';
+import parameter4 from '@assets/images/Parameter/parameter4.svg';
+
+const data = [
+  {
+    id: 1,
+    img: parameter1,
+    name: 'Температура',
+    value: '20° - ощущается как 17°',
+  },
+  {
+    id: 2,
+    img: parameter1,
+    name: 'Температура',
+    value: '20° - ощущается как 17°',
+  },
+  {
+    id: 3,
+    img: parameter1,
+    name: 'Температура',
+    value: '20° - ощущается как 17°',
+  },
+  {
+    id: 4,
+    img: parameter1,
+    name: 'Температура',
+    value: '20° - ощущается как 17°',
+  },
+];
+
 export default function DescriptionCurrentDate() {
   return (
     <div className={style.description}>
-      <ul>
+      <ul className={style.list}>
         <li>
-          <Parametr />
+          <Parametr
+            img={parameter1}
+            name="Температура"
+            value="20° - ощущается как 17°"
+          />
         </li>
         <li>
-          <Parametr />
+          <Parametr
+            img={parameter2}
+            name="Давление "
+            value="765 мм ртутного столба - нормальное"
+          />
         </li>
         <li>
-          <Parametr />
+          <Parametr
+            img={parameter3}
+            name="Осадки"
+            value="Без осадков"
+          />
         </li>
         <li>
-          <Parametr />
+          <Parametr
+            img={parameter4}
+            name="Ветер"
+            value="3 м/с юго-запад - легкий ветер"
+          />
         </li>
       </ul>
     </div>
--- src/components/UI/Parameter/Parameter.tsx
+++ src/components/UI/Parameter/Parameter.tsx
@@ -1,5 +1,19 @@
 import style from './Parameter.module.scss';
 
-export default function Parametr() {
-  return <div className={style.parameter}>Параметр</div>;
+interface IProps {
+  img: string;
+  name: string;
+  value: string;
+}
+
+export default function Parametr({ img, name, value }: IProps) {
+  return (
+    <div className={style.parameter}>
+      <div className={style.img}>
+        <img src={img} alt="" />
+      </div>
+      <p className={style.name}>{name}</p>
+      <p className={style.value}>{value}</p>
+    </div>
+  );
 }
